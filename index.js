d261be8880652c122f9577da55d9ec7c8c63d524
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-03 17:12:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://pornhub.com';
+const nggUrl = 'https://beta.character.ai';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'pornhub.com') {
+    if (req.headers.host === 'beta.character.ai') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
